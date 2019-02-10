9683649039e52e3824609bd21a9ba3b75b4916d3
nelsonj15 <30556952+nelsonj15@users.noreply.github.com>
2019-02-10 11:59:40 -0600

    Update Survivor.java

--- ZombieWar/src/edu/csp/csc422/Survivor.java
+++ ZombieWar/src/edu/csp/csc422/Survivor.java
@@ -1,34 +1,36 @@
- * Concordia CSC422 - Zombie War Group Assignment.
- * by Diana Arita, Rob Nelson, Andrew Nielsen, Dean Peterson.
- * 2019-02-10
- */
-package edu.csp.csc422;
+ /* 
+  *Concordia CSC422 - Zombie War Group Assignment.
+  * by Diana Arita, Rob Nelson, Andrew Nielsen, Dean Peterson.
+  * 2019-02-10
+  *
+  */
+ package edu.csp.csc422;
 
-public abstract class Survivor {
-    private int health;
-    private int damage;
-    private String name;
-    
-    public Survivor(String name) {
-        this.name = name;
-    }
-    
-    public int getHealth() {
-        return health;
-    }
-    public void setHealth(int health) {
-        this.health = health;
-    }
-    public int getDamage() {
-        return damage;
-    }
-    public void setDamage(int damage) {
-        this.damage = damage;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-}
+ public abstract class Survivor {
+     private int health;
+     private int damage;
+     private String name;
+
+     public Survivor(String name) {
+         this.name = name;
+     }
+
+     public int getHealth() {
+         return health;
+     }
+     public void setHealth(int health) {
+         this.health = health;
+     }
+     public int getDamage() {
+         return damage;
+     }
+     public void setDamage(int damage) {
+         this.damage = damage;
+     }
+     public String getName() {
+         return name;
+     }
+     public void setName(String name) {
+         this.name = name;
+     }
+ }
